a12bee0509877c26d49bc22323b7325dbb10f4f7
katana8023 <katana8023@163.com>
2020-04-28 15:01:25 +0800

    GitTest.java upload

---
+++ src/main/java/com/gx/git/pojo/User.java
@@ -0,0 +1,32 @@
+package com.gx.git.pojo;
+
+public class User {
+
+    private String username;
+    private String tid;
+    private int age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTid() {
+        return tid;
+    }
+
+    public void setTid(String tid) {
+        this.tid = tid;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/gx/git/service/LoginService.java
@@ -0,0 +1,11 @@
+package com.gx.git.service;
+
+import com.gx.git.pojo.User;
+
+public class LoginService {
+
+    public void doLogin() {
+        System.out.printf(" User is logging in...");
+    }
+
+}
